2d009f8cde57fe0144adcd19348102681433a014
Tayte Waterman <tayte.d.waterman@gmail.com>
2025-11-02 13:03:04 +0100

    Lab week 6 exercises 6-7

--- 07-javascript/06-fetch.js
+++ 07-javascript/06-fetch.js
@@ -1,9 +1,68 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+const app = document.querySelector("#books");
+const loading = document.querySelector("#loading");
+
+const buildBook = (bookData) => {
+  // Destructure book info from json
+  const { name, authors, released, numberOfPages } = bookData;
+
+  // Create root book div
+  const book = document.createElement("div");
+  book.setAttribute("class", "book text-center");
+
+  // Create title div
+  const title = document.createElement("div");
+  title.setAttribute("class", "fs-4");
+  title.textContent = name;
+
+  // Create author div
+  const author = document.createElement("div");
+  author.setAttribute("class", "mb-3");
+  author.textContent = `by ${authors[0]}`; // Assume only one author/only display 1st author
+
+  // Create publication year div
+  const year = document.createElement("div");
+  year.setAttribute("class", "mb-3");
+  year.textContent = released.split("-")[0]; // Assume data always in format: YYYY-MM-DDTHH:MM:SS
+
+  // Create page-length div
+  const pages = document.createElement("div");
+  pages.setAttribute("class", "mb-3");
+  pages.textContent = `${numberOfPages} pages`;
+
+  // Insert child elements and return
+  book.append(title, author, year, pages);
+  return book;
+};
 
 const fetchData = (url) => {
   // Fetch all books from the API of Ice and Fire and append them to the DOM
   // Create an element for each book that contains title, author, publication year, and number of pages
   // Update the styles in JavaScript to center all the books in the container given
+
+  fetch(url)
+    .then((response) => {
+      // Check if valid response and then convert to json
+      if (!response) {
+        throw new Error(`Unexpected response: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Remove loading element
+      loading.remove();
+
+      // Parse book data and insert to dom
+      for (bookData of data) {
+        app.append(buildBook(bookData));
+      }
+    })
+    .catch((error) => {
+      // Handle errors/failures
+      console.error(`Enexpected failure in fetch(${url}): ${error}`);
+    });
 };
+
+// Call fetchData()
+fetchData(url);
--- 07-javascript/07-functional.js
+++ 07-javascript/07-functional.js
@@ -1,41 +1,63 @@
 const characters = [
-  { id: 1, name: 'Arya', house: 'Stark' },
-  { id: 4, name: 'Sansa', house: 'Stark' },
-  { id: 16, name: 'Eddard', house: 'Stark' },
-  { id: 17, name: 'Catelyn', house: 'Stark' },
-  { id: 20, name: 'Robb', house: 'Stark' },
-  { id: 28, name: 'Rickon', house: 'Stark' },
-  { id: 40, name: 'Jaime', house: 'Lannister' },
-  { id: 41, name: 'Cersei', house: 'Lannister' },
-  { id: 42, name: 'Tyrion', house: 'Lannister' },
-  { id: 50, name: 'Tywin', house: 'Lannister' },
-  { id: 53, name: 'Kevan', house: 'Lannister' },
-  { id: 75, name: 'Daenerys', house: 'Targaryen' },
-  { id: 77, name: 'Viserys', house: 'Targaryen' },
-  { id: 81, name: 'Rhaegar', house: 'Targaryen' },
-  { id: 83, name: 'Theon', house: 'Greyjoy' },
-  { id: 84, name: 'Yara', house: 'Greyjoy' },
-  { id: 87, name: 'Davos', house: 'Seaworth' },
-  { id: 88, name: 'Brienne', house: 'Tarth' },
+  { id: 1, name: "Arya", house: "Stark" },
+  { id: 4, name: "Sansa", house: "Stark" },
+  { id: 16, name: "Eddard", house: "Stark" },
+  { id: 17, name: "Catelyn", house: "Stark" },
+  { id: 20, name: "Robb", house: "Stark" },
+  { id: 28, name: "Rickon", house: "Stark" },
+  { id: 40, name: "Jaime", house: "Lannister" },
+  { id: 41, name: "Cersei", house: "Lannister" },
+  { id: 42, name: "Tyrion", house: "Lannister" },
+  { id: 50, name: "Tywin", house: "Lannister" },
+  { id: 53, name: "Kevan", house: "Lannister" },
+  { id: 75, name: "Daenerys", house: "Targaryen" },
+  { id: 77, name: "Viserys", house: "Targaryen" },
+  { id: 81, name: "Rhaegar", house: "Targaryen" },
+  { id: 83, name: "Theon", house: "Greyjoy" },
+  { id: 84, name: "Yara", house: "Greyjoy" },
+  { id: 87, name: "Davos", house: "Seaworth" },
+  { id: 88, name: "Brienne", house: "Tarth" },
 ];
 
 const mapNameFamily = (characters) => {
   // Return an array with the name and house of each character
   // Format: "Arya, of House Stark"
 
-  return characters;
+  const results = [];
+  for (character of characters) {
+    results.push(`${character.name}, of House ${character.house}`);
+  }
+
+  return results;
 };
 
 const filterFamily = (characters, house) => {
   // Return an array with only the characters from a given house
 
-  return characters;
+  results = [];
+  for (character of characters) {
+    if (character.house == house) {
+      results.push(character);
+    }
+  }
+
+  return results;
 };
 
 const reduceHouses = (characters) => {
   // Return an object with the number of characters from each house
 
-  return characters;
+  results = {};
+  for (character of characters) {
+    const { house } = character;
+    if (!results.hasOwnProperty(house)) {
+      // If key doesn't exist, add it
+      results[house] = 0;
+    }
+    results[house] += 1; // Update count
+  }
+
+  return results;
 };
 
 console.log(mapNameFamily(characters));
@@ -60,7 +82,7 @@ console.log(mapNameFamily(characters));
 //   'Brienne, of House Tarth'
 // ]
 
-console.log(filterFamily(characters, 'Stark'));
+console.log(filterFamily(characters, "Stark"));
 // [
 //   { id: 1, name: 'Arya', house: 'Stark' },
 //   { id: 4, name: 'Sansa', house: 'Stark' },
@@ -70,7 +92,7 @@ console.log(filterFamily(characters, 'Stark'));
 //   { id: 28, name: 'Rickon', house: 'Stark' }
 // ]
 
-console.log(filterFamily(characters, 'Greyjoy'));
+console.log(filterFamily(characters, "Greyjoy"));
 // [
 //   { id: 83, name: 'Theon', house: 'Greyjoy' },
 //   { id: 84, name: 'Yara', house: 'Greyjoy' }
